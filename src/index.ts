3a7d5504a07b8b9425c5e3a86bd6e101e00df5f0
Björn Tegelund <bjorn@tumblbug.com>
2017-11-14 14:28:01 +0900

    Add Result, Meaning exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import search from './searcher'
-import scrape, {Result} from './scraper'
+import scrape, { Result } from './scraper'
 
 export default (term: string): Promise<Result[]> => search(term).then(scrape)
+
+export { Result, Meaning } from './scraper'
